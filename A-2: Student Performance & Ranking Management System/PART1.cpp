d66a342c38c93cc9630434a5877ff6079f35c594
garvsinha01 <garvsinha01@gmail.com>
2026-09-17 19:52:23 +0530

    Update PART1.cpp

--- A-2: Student Performance & Ranking Management System/PART1.cpp
+++ A-2: Student Performance & Ranking Management System/PART1.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-    int rollNo[6];
+    int rollNo[5];
 
     cout << "Enter Roll Numbers of 5 students:\n";
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 5; i++) {
         cin >> rollNo[i];
     }
 
     cout << "\nStudent Roll Numbers:\n";
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 5; i++) {
         cout << rollNo[i] << endl;
     }
 
