969047e5e0a1228875376f559cb3bf4de276fdc4
garvsinha01 <garvsinha01@gmail.com>
2026-09-17 23:50:49 +0530

    Update PART1.cpp

--- A-5: Student Performance Record Management System/PART1.cpp
+++ A-5: Student Performance Record Management System/PART1.cpp
@@ -3,29 +3,16 @@ using namespace std;
 
 int main() {
     int rollNo[5];
-    int search, i;
-    bool found = false;
 
-    cout << "Enter roll numbers of 5 students: ";
-
-    for(i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++) {
         cin >> rollNo[i];
     }
 
-    cout << "Enter roll number to search: ";
-    cin >> search;
+    cout << "\nStudent RollNo:" << endl;
 
-    for(i = 0; i < 5; i++) {
-        if(rollNo[i] == search) {
-            found = true;
-            break;
-        }
+    for (int i = 0; i < 5; i++) {
+        cout << rollNo[i] << endl;
     }
-
-    if(found)
-        cout << "Roll number found";
-    else
-        cout << "Roll number not found";
 
     return 0;
 }
